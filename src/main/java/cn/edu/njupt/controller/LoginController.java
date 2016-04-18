fd6f4715e022e9147e5ad3d168decce55c9032bc
高翔 <15895979133@163.com>
2016-04-18 19:30:13 +0800

    恢复 logincontroller
    恢复 logincontroller

--- src/main/java/cn/edu/njupt/controller/LoginController.java
+++ src/main/java/cn/edu/njupt/controller/LoginController.java
@@ -1,10 +1,15 @@
 package cn.edu.njupt.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -16,6 +21,7 @@ import cn.edu.njupt.model.UserRole;
 import cn.edu.njupt.service.RoleServiceI;
 import cn.edu.njupt.service.SystemDDLServiceI;
 import cn.edu.njupt.service.UserServiceI;
+import cn.edu.njupt.util.LogonUtils;
 
 @Controller
 public class LoginController {
@@ -30,13 +36,31 @@ public class LoginController {
 	private SystemDDLServiceI  systemDDLService;
 	
 	
+	@SuppressWarnings("deprecation")
 	@RequestMapping("/loginIndex.do")
-	public String loginIndex(){
-		return "login";
+	public String loginIndex(HttpServletRequest request,
+						HttpServletResponse response,ModelMap map){
+		//1.读取本地cookie值
+	    Cookie cookie[] = request.getCookies();
+		if(cookie!=null && cookie.length>0){
+	    	 for (Cookie cookie2 : cookie) {
+		 	    	if(null!=cookie2 && "logonname".equals(cookie2.getName()))
+		 	    	{
+		 	    		map.put("logonname", URLDecoder.decode(cookie2.getValue()));
+		 	    		map.put("checked", "checked");
+	 	 	    		
+		 	    	}
+		 	    	if(null!=cookie2 && "logonpwd".equals(cookie2.getName()))
+		 	    	{
+		 	    		map.put("logonpwd", cookie2.getValue());
+		 	    	}
+	 	 		}
+		  }
+	  	return "login";
 	}
 	
 	@RequestMapping("/login.do")
-	public String login(String logonname,String logonpwd,HttpServletRequest request,ModelMap map){
+	public String login(String logonname,String logonpwd,HttpServletResponse response,HttpServletRequest request,ModelMap map){
 		//验证登陆
 		User user = userServiceI.findUserExist(logonname);
 		if(null!= user)
@@ -85,9 +109,22 @@ public class LoginController {
 			map.put("message", "不存在该用户！");
 			return "login";
 		}
+		//记住我的功能
+		try {
+				LogonUtils.remeberMeByCookie(request, response);
+				} catch (UnsupportedEncodingException e) {
+					e.printStackTrace();
+			}
 		
 		return "redirect:home/spotlightAdmin.do";
 	}
+	
+	
+ 	@RequestMapping("/exit.do")
+	public String exit(HttpSession  httpSession){
+ 		httpSession.invalidate();
+		return "redirect:loginIndex.do";
+ 	}
 	
 	@RequestMapping("/register.do")
 	public String register(){
